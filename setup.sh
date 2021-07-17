0ec6b660120c3e79f96091cfca8208da6e8c5717
Hudson Barroso <hudson.g4@gmail.com>
2021-07-16 22:17:00 -0400

    Second Commit

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
-mkdir -p ~/.streamlit/
-
-echo "\[general]\n\email = \"email@domain\"\n\" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
